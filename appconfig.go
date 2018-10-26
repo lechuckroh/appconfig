a1850ef9b872833597cbc93b5d22f2a20476f38b
Lechuck Roh <member@lechuck2011.cafe24.com>
2018-10-26 17:11:55 +0900

    init with go module

---
+++ appconfig.go
@@ -0,0 +1,12 @@
+package appconfig
+
+// ActiveProfileEnvName represents environment name for active profile
+var ActiveProfileEnvName = "app.profiles.active"
+
+// ConfigFilenamePrefix represents configuration filename prefix
+var ConfigFilenamePrefix = "application"
+
+// LoadConfig loads configuration
+func LoadConfig(configFilename string) (interface{}, error) {
+	return nil, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lechuckroh/appconfig
+
+require github.com/heetch/confita v0.5.1 // indirect
